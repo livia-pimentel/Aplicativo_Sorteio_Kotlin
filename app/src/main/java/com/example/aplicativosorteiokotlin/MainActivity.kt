9d9494173570c0589d71dac495c32ffe7f1e3624
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-03-19 08:59:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aplicativosorteiokotlin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.aplicativosorteiokotlin
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortear(view: View) {
+
+        val textResultdo = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random.nextInt(11) // 0 1 2 3 4 5 6 7 8 9 10
+
+        textResultdo.setText("Número Sorteado: $numero")
+
+    }
+
+}
